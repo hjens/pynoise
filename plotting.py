0fd0bf03268e2d9795fa85975d679567a5c4c9fc
Hannes Jensen <hjens@astro.su.se>
2012-10-30 16:22:15 +0100

    added plot_image_slice function. fixed colorbars

--- plotting.py
+++ plotting.py
@@ -6,6 +6,8 @@ def plot_visibility_slice(parameters, visibility_slice = None, **kwargs):
 	'''
 	Make an image plot of a visibility slice.
 
+	Only the real part is plotted
+
 	Parameters:
 		* parameters (Parameters structure) --- the structure holding the 
 			parameters
@@ -27,11 +29,48 @@ def plot_visibility_slice(parameters, visibility_slice = None, **kwargs):
 	extent = [-uvrange/2., uvrange/2., -uvrange/2., uvrange/2.]
 
 	#Generate title string
-	title = 'Visibility noise for %d hrs observation @ %.1f MHz' % (parameters.get_t(), parameters.get_nu_c())
+	title = 'Visibility noise for %d hrs observation @ %.1f MHz' % (parameters.get_t(), 
+			parameters.get_nu_c())
 
 	#Plot it
 	pl.imshow(visibility_slice, extent=extent, **kwargs)
 	pl.xlabel('$u/\lambda$')
 	pl.ylabel('$v/\lambda$')
-	pl.colorbar()
+	cbar = pl.colorbar()
+	cbar.set_label('mK')
+	pl.title(title)
+
+
+def plot_image_slice(parameters, image_slice = None, **kwargs):
+	'''
+	Make an image plot of a image slice.
+
+	Parameters:
+		* parameters (Parameters structure) --- the structure holding the 
+			parameters
+	Kwargs:
+		* image_slice = None --- the slice to plot. If none is given, a
+			slice is generated from the Parameters structure.
+
+	Additional kwargs are passed to imshow
+	'''
+
+	#Fix the image slice to plot
+	if image_slice == None:
+		image_slice = parameters.get_image_slice()
+
+	#Get the extent
+	fov = parameters.get_fov()
+	extent = [-fov/2., fov/2., -fov/2., fov/2.]
+
+	#Generate title string
+	title = 'Image noise for %d hrs observation @ %.1f MHz' % (parameters.get_t(), 
+			parameters.get_nu_c())
+
+	#Plot it
+	pl.imshow(image_slice, extent=extent, **kwargs)
+	pl.xlabel('Degrees')
+	pl.ylabel('Degrees')
+	cbar = pl.colorbar()
+	cbar.set_label('mK')
 	pl.title(title)
